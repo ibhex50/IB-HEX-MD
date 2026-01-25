16473e5e9f62f6176d6a9758df8e08f687a7119c
IB HEX <ibhex50@gmail.com>
2026-01-25 02:57:20 +0000

    Update slap.js

--- commands/slap.js
+++ commands/slap.js
@@ -1,10 +1,16 @@
+const axios = require("axios");
+
 module.exports = {
   name: "slap",
-  desc: "Gifler quelqu’un",
+  desc: "Gif anime slap",
   async execute(sock, m) {
     const t = m.mentionedJid?.[0];
+    const { data } = await axios.get("https://api.waifu.pics/sfw/slap");
+
     await sock.sendMessage(m.key.remoteJid, {
-      text: t ? `👋 @${t.split("@")[0]} a été giflé 😭` : "👋 Slap !",
+      video: { url: data.url },
+      gifPlayback: true,
+      caption: t ? `👋 @${t.split("@")[0]} SLAP 💥` : "👋 SLAP 💥",
       mentions: t ? [t] : []
     });
   }
