2105e0c83549fd4a65ca78c170efc84f12398473
IB HEX <ibhex50@gmail.com>
2026-01-25 03:10:04 +0000

    Update happy.js

--- commands/happy.js
+++ commands/happy.js
@@ -2,11 +2,14 @@ const axios = require("axios");
 
 module.exports = {
   name: "happy",
+  desc: "Gif anime happy",
   async execute(sock, m) {
     const { data } = await axios.get("https://api.waifu.pics/sfw/happy");
 
     await sock.sendMessage(m.key.remoteJid, {
-      sticker: { url: data.url }
+      video: { url: data.url },
+      gifPlayback: true,
+      caption: "😄 Happy mood ✨"
     });
   }
 };
